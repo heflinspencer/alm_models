2645b6310f3df3226810360309a58df30988386d
Spencer Heflin <heflinspencer@gmail.com>
2026-08-26 21:25:42 +0700

    chore: expand behavioral_models architecture to include prepayments, breakage, drawdowns, and pipeline modeling

---
+++ src/behavioral_models/breakage_model.py
@@ -0,0 +1,6 @@
+class TermDepositBreakageModel:
+    """
+    Models the probability of a customer breaking a term deposit early,
+    comparing the early withdrawal period against the reinvestment opportunity cost.
+    """
+    pass
---
+++ src/behavioral_models/prepayment_model.py
@@ -0,0 +1,6 @@
+class MortgagePrepaymentModel:
+    """
+    Predicts Conditional Prepayment Rates (CPR) for fixed-rate mortgages
+    based on the spread between the contractual rate and current market rates (refinancing incentive).
+    """
+    pass
